663f078118a427596329111420e28dfc9d5d5cfb
MundiMed <mundimed.adm@gmail.com>
2024-06-25 12:17:45 -0300

    dev

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,11 @@ use App\Http\Controllers\ImportController;
 Route::controller(ImportController::class)->group(function () {
     Route::get('truncate', [ImportController::class, 'truncateTables']);
     Route::get('migrate', [ImportController::class, 'index']);
+    Route::get('migrate1', [ImportController::class, 'index1']);
+    Route::get('migrate2', [ImportController::class, 'index2']);
+    Route::get('migrate3', [ImportController::class, 'index3']);
+    Route::get('migrate4', [ImportController::class, 'index4']);
+    Route::get('migrate5', [ImportController::class, 'index5']);
     Route::get('insert_approved_service_order_itens', [ImportController::class, 'callSPInsertApprovedServiceOrderItens']);
     Route::get('insert_approved_service_orders', [ImportController::class, 'callSPInsertApprovedServiceOrders']);
     Route::get('insert_accredited_suppliers', [ImportController::class, 'callSPInsertAccreditedSuppliers']);
