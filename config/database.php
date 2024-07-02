653584d1e2dadaacf2025f7789956a98d833e0cb
MundiMed <mundimed.adm@gmail.com>
2024-07-02 12:00:28 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1099,13 +1099,13 @@ return [
         'mysql_mundimed_v1' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            /*
+            //*
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'mundimed_v1'),
+            'database' => env('DB_DATABASE', 'mundimed_unificada'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'Mechatron@2024'),
-            //*/
+            /*/
             'host' => env('DB_HOST', '187.102.244.36'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'medmecom_mundimed_unificada'),
